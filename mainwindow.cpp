b18c328a479272b0f82373b8ff446f56e8062e0f
Andrey-Saenkov <107579349+Andrey-Saenkov@users.noreply.github.com>
2022-06-20 16:06:49 +0200

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,13 @@
 #include "mainwindow.h"                                                                                                                                                                                                   #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QPainter>
-#include <QMouseEvent> // мы его подключаем, потому что используем его внутренности
+#include <QMouseEvent>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    //(1)valid = false;
 }
 
 MainWindow::~MainWindow()
@@ -16,71 +15,54 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::paintEvent(QPaintEvent *){//главное, чтобы тип у * совпадал с типом * из mainwindow.h
+void MainWindow::paintEvent(QPaintEvent *)
+{
    QPainter painter(this);
-   //qDebug("paintEvent size = %d", points.size());
    for (int g = 0; g < points.size(); g++){
-       //qDebug("Рисую %d точку", g);
-       points[g].drawPoint(&painter); //& это ссылка(не в Сибирь)
+       points[g].drawPoint(&painter);
        if (g != points.size() - 1){
            painter.drawLine(points[g].getX(),points[g].getY(),points[g + 1].getX(),points[g + 1].getY());
        }
    }
 
 }
-void MainWindow::mousePressEvent(QMouseEvent *event) // из параметра(*event) можно извлечь полезные шутки. Из него также можно получить модификатроры, которые были использованы во время клика(ctrl,shift,alt)
+void MainWindow::mousePressEvent(QMouseEvent *event)
 {
     bool hit = false;
     int g = 0;
-    for (; g < points.size(); g++){
-        if (points[g].distance2(event->pos().x(), event->pos().y()) < 144){
+    for (; g < points.size(); g++)
+        if (points[g].distance2(event->pos().x(), event->pos().y()) < 144) {
             hit = true;
             break;
         }
+    if (hit) {
+        if (event->button() & Qt::RightButton)
+            points.remove(g);
+        else {
+            movingPointInd = g;
+            hitPoint = Point(event->pos().x(), event->pos().y());
+        }
     }
-    if (hit){
-       //int w = points[g].getWidth();
-       //points[g].setWidth(w * 2);
-       movingPointInd = g;
-       hitPoint = Point(event->pos().x(), event->pos().y());
-       points.remove(g);
-       qDebug("Захвачена точка %d", g);
-    }
-    else{
-        qDebug("Добавить");
+    else
         points.append(Point(event->pos().x(), event->pos().y()));
-        //qDebug("points = %f %f %d %d",points);
-    }
-    repaint();
-    //update();
+    update();
 }
 void MainWindow::mouseReleaseEvent(QMouseEvent *event)
 {
-    //qDebug("Release x = %d y = %d",event->x(),event->y());
+    movingPointInd = -1;
 }
 
-void MainWindow::mouseMoveEvent(QMouseEvent *event)//event-> x(), event-> y()
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
 {
     if (movingPointInd == -1) return ;
-    int dx = 0, dy = 0;
-    event->x(), event->y();
-    hitPoint.getX(),hitPoint.getY();
-    dx = event->x() - hitPoint.getX();
-    dy = event->y() - hitPoint.getY();
-    qDebug("dx = %d, dy = %d", dx, dy);
-    qDebug("size = %d, movingPointInd = %d",points.size(), movingPointInd);
-    int x = points[movingPointInd].getX();
+    int dx, dy;
+    dx = event->pos().x() - hitPoint.getX();
+    dy = event->pos().y() - hitPoint.getY();
     points[movingPointInd].setX(points[movingPointInd].getX() + dx);
-    int y = points[movingPointInd].getY();
     points[movingPointInd].setY(points[movingPointInd].getY() + dy);
-    qDebug("Захвачена точка %d",movingPointInd);
-    hitPoint.setX(event->x());
-    hitPoint.setY(event->y());
-    //update();
-    repaint();
-    qDebug("Move x = %d y = %d",event->x(),event->y());
+    hitPoint.setX(event->pos().x());
+    hitPoint.setY(event->pos().y());
+    update();
 }
-
-
 
 
